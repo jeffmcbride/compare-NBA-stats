2ff69f57dba12838c9dacf7d646bb4b0e41c398d
Jeff McBride <jeffmcbride@live.com>
2020-01-06 15:17:32 -0800

    added player

--- nba.py
+++ nba.py
@@ -1,46 +1,63 @@
-import matplotlib.pyplot as plt
+import plotly
 from nba_api.stats.endpoints import teamyearbyyearstats
-from nba_api.stats.static import teams
+from nba_api.stats.endpoints import playercareerstats
+from nba_api.stats.static import *
 
 
+teams.find_teams_by_full_name("ca")
 
 '''Available team stats:
-    TEAM_ID, TEAM_CITY, TEAM_NAME, YEAR, GP, WINS, LOSSES, WIN_PCT, CONF_RANK, 
-    DIV_RANK, PO_WINS, PO_LOSSES, CONF_COUNT, DIV_COUNT, NBA_FINALS_APPEARANCE, 
-    FGM, FGA, FG_PCT, FG3M, FG3A, FG3_PCT, FTM, FTA, FT_PCT, OREB, DREB, REB, 
+    TEAM_ID, TEAM_CITY, TEAM_NAME, YEAR, GP, WINS, LOSSES, WIN_PCT, CONF_RANK,
+    DIV_RANK, PO_WINS, PO_LOSSES, CONF_COUNT, DIV_COUNT, NBA_FINALS_APPEARANCE,
+    FGM, FGA, FG_PCT, FG3M, FG3A, FG3_PCT, FTM, FTA, FT_PCT, OREB, DREB, REB,
     AST, PF, STL, TOV, BLK, PTS, PTS_RANK'''
-    
+
 class Team:
-    
-    def __init__(self, name, start, end):
+
+    def __init__(self, name):
         self.search_name = name
-        self.start = start
-        self.end = end
-        self.duration = end - start
-        self.teamid = teams.find_teams_by_full_name(self.search_name)[0]['id']
-        self.teamstats = teamyearbyyearstats.TeamYearByYearStats(self.teamid).get_data_frames()
-        self.teamname = self.teamstats[0]['TEAM_NAME'].iloc[-1]
-        
-        
-    def get_stats(self, stat):
-        return self.teamstats[0].set_index("YEAR")[stat]
-        
-        
-    def graph_stats(self, stat):
-        stats = self.get_stats(stat)
+        self.team = teams.find_teams_by_full_name(self.search_name)[0]
+        self.teamid = self.team['id']
+        self.teamname = self.team['nickname']
+        self.teamstats = teamyearbyyearstats.TeamYearByYearStats(self.teamid).get_data_frames()[0]
+
+    def get_stats(self, stat, start, end):
+        stats_in_range = self.teamstats[self.teamstats['YEAR'] <= end]
+        stats_in_range = stats_in_range[self.teamstats['YEAR'] >= start]
+        return stats_in_range.set_index("YEAR")[stat]
+
+
+    def graph_stats(self, stat, start, end):
+        stats = self.get_stats(stat, start, end)
         ax = stats.plot()
         ax.legend([self.teamname + ' ' + stat]);
-        
-        
-        
-def graphTwoTeams(Team1, Team2, stat):
-    # Create dataframe of years and stat
-    pass
 
 
-        
-raps = Team('rap', 5, 4)
-raps.graph_stats('WINS')
+'''Available player stats:
+    PLAYER_ID, SEASON_ID, LEAGUE_ID, TEAM_ID, TEAM_ABBREVIATION, PLAYER_AGE,
+    GP, GS, MIN, FGM, FGA, FG_PCT, FG3M, FG3A, FG3_PCT, FTM, FTA, FT_PCT, OREB,
+    DREB, REB, AST, STL, BLK, TOV, PF, PTS'''
+
+class Player:
+
+    def __init__(self, name):
+        self.search_name = name
+        self.player = players.find_players_by_full_name(self.search_name)[0]
+        self.playerid = self.player['id']
+        self.playername = self.player['full_name']
+        self.playerstats = playercareerstats.PlayerCareerStats(self.playerid).get_data_frames()[0]
+
+    def get_stats(self, stat, start, end):
+        stats_in_range = self.playerstats[self.playerstats['SEASON_ID'] <= end]
+        stats_in_range = stats_in_range[self.playerstats['SEASON_ID'] >= start]
+        return stats_in_range.set_index("SEASON_ID")[stat]
+
+    def graph_stats(self, stat, start, end):
+        stats = self.get_stats(stat, start, end)
+        ax = stats.plot()
+        ax.legend([self.playername + ' ' + stat]);
+
+
 
 
 
